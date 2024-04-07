21525b7a850d93214b6a44d3f5fa217e8120e1ef
Ahmed Aladdin <aamieeu@gmail.com>
2024-04-07 08:58:06 +0200

    follow our naming conventions

--- src/test_generator.c
+++ src/test_generator.c
@@ -5,9 +5,9 @@
 
 struct processData
 {
-    int arrivaltime;
+    int arrival_time;
     int priority;
-    int runningtime;
+    int running_time;
     int id;
 };
 
@@ -22,16 +22,16 @@ int main(int argc, char * argv[])
     srand(time(null));
     //fprintf(pFile,"%d\n",no);
     fprintf(pFile, "#id arrival runtime priority\n");
-    pData.arrivaltime = 1;
+    pData.arrival_time = 1;
     for (int i = 1 ; i <= no ; i++)
     {
         //generate Data Randomly
         //[min-max] = rand() % (max_number + 1 - minimum_number) + minimum_number
         pData.id = i;
-        pData.arrivaltime += rand() % (11); //processes arrives in order
-        pData.runningtime = rand() % (30);
+        pData.arrival_time += rand() % (11); //processes arrives in order
+        pData.running_time = rand() % (30);
         pData.priority = rand() % (11);
-        fprintf(pFile, "%d\t%d\t%d\t%d\n", pData.id, pData.arrivaltime, pData.runningtime, pData.priority);
+        fprintf(pFile, "%d\t%d\t%d\t%d\n", pData.id, pData.arrival_time, pData.running_time, pData.priority);
     }
     fclose(pFile);
 }
