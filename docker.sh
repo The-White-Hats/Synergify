54b58114eba8b9e0e2e2c183dab05a9917695ef4
Ahmed Aladdin <aamieeu@gmail.com>
2024-04-08 17:46:06 +0200

    ⭐ Set up the project to use docker 🐳

---
+++ docker.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+# this bash script is meant to run on ubuntu
+#? to run it on windows, use a bash command line like git bash
+ImageName=synergify
+flag=$(docker images | grep $ImageName-image)
+
+# Check if the 'flag' variable is empty
+# that means that there is no an image with such a name
+if [[ -z $flag ]]; then
+  docker build -f ./docker/Dockerfile -t synergify-image .
+fi
+
+docker compose -f ./docker/docker-compose.yml up -d
+docker attach $ImageName-container
