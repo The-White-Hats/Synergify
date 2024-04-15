ed63f931a001d56179aa0003a15e905f6e6a7631
Ahmed Aladdin <aamieeu@gmail.com>
2024-04-15 22:35:58 +0200

    Refactor headers.h file by adding include guards

--- src/headers.h
+++ src/headers.h
@@ -1,4 +1,7 @@
-#include <stdio.h>      //if you don't use scanf/printf change this include
+#ifndef SAMPLE_HEADER_H
+#define SAMPLE_HEADER_H
+
+#include <stdio.h> //if you don't use scanf/printf change this include
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/file.h>
@@ -17,22 +20,23 @@ typedef short bool;
 
 #define SHKEY 300
 
-
 ///==============================
-//don't mess with this variable//
-int * shmaddr;                 //
+// don't mess with this variable//
+int *shmaddr; //
 //===============================
 
 ///==============================
 // Structs & Enums
-typedef struct process_info_s {
+typedef struct process_info_s
+{
     int id;
     int arrival;
     int runtime;
     int priority;
-} process_info_t ;
+} process_info_t;
 
-typedef enum {
+typedef enum
+{
     HPF = 1,
     SRTN,
     RR
@@ -44,24 +48,22 @@ int getClk()
     return *shmaddr;
 }
 
-
 /*
  * All process call this function at the beginning to establish communication between them and the clock module.
  * Again, remember that the clock is only emulation!
-*/
+ */
 void initClk()
 {
     int shmid = shmget(SHKEY, 4, 0444);
     while ((int)shmid == -1)
     {
-        //Make sure that the clock exists
+        // Make sure that the clock exists
         printf("Wait! The clock not initialized yet!\n");
         sleep(1);
         shmid = shmget(SHKEY, 4, 0444);
     }
-    shmaddr = (int *) shmat(shmid, (void *)0, 0);
+    shmaddr = (int *)shmat(shmid, (void *)0, 0);
 }
-
 
 /*
  * All process call this function at the end to release the communication
@@ -69,7 +71,7 @@ void initClk()
  * Again, Remember that the clock is only emulation!
  * Input: terminateAll: a flag to indicate whether that this is the end of simulation.
  *                      It terminates the whole system and releases resources.
-*/
+ */
 
 void destroyClk(bool terminateAll)
 {
@@ -79,3 +81,5 @@ void destroyClk(bool terminateAll)
         killpg(getpgrp(), SIGINT);
     }
 }
+
+#endif /* SAMPLE_HEADER_H */
