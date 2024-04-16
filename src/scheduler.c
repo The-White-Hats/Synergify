c47e2bcb958d25b0c3e94daf15c6cf279aaab18a
Mo2Hefny <moamensaied1@gmail.com>
2024-04-16 02:45:27 +0200

    feat(scheduler): add scheduling algorithms
    Fix algorithms implementation and add a Singleton instance for scheduler configuration.

--- src/scheduler.c
+++ src/scheduler.c
@@ -4,6 +4,9 @@
 int main(int argc, char * argv[])
 {
     initClk();
+    SchedulerConfig* schedulerConfig = getSchedulerConfigInstance();
+    void (*scheduleFunction[])(pqueue_t **, rprocess_t *) = {scheduleHPF, scheduleSRTN, scheduleRR};
+    int selectedAlgorithmIndex = schedulerConfig->selected_algorithm - 1;
     
     //TODO implement the scheduler :)
     //upon termination release the clock resources.
