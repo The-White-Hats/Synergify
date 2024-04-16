3f2e5b171704d0442ecbb355778eff8b20d550ab
Mo2Hefny <moamensaied1@gmail.com>
2024-04-16 02:45:27 +0200

    feat(scheduler): add scheduling algorithms
    Fix algorithms implementation and add a Singleton instance for scheduler configuration.

---
+++ src/process_util.c
@@ -0,0 +1,11 @@
+#include "headers.h"
+
+/**
+ * decrementRemainingCPUTime - Decrement the remaining CPU time of a process.
+ * @param process: Pointer to the process information structure.
+ */
+void decrementRemainingCPUTime(process_info_t* process) {
+  process->runtime--;
+}
+
+// Calculate other data useful for logging
