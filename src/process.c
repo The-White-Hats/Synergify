8df518fd59e9d04350634e67df1d19007beb19e1
Ahmed Aladdin <aamieeu@gmail.com>
2024-04-21 23:19:06 +0200

    feat(process): write the "process" program logic

--- src/process.c
+++ src/process.c
@@ -1,20 +1,39 @@
 #include "headers.h"
 
 /* Modify this file as needed*/
-int remainingtime;
+int remaining_time;
+int prev_time;
 
-int main(int agrc, char * argv[])
+int main(int argc, char *argv[])
 {
-    initClk();
-    
-    //TODO it needs to get the remaining time from somewhere
-    //remainingtime = ??;
-    while (remainingtime > 0)
+    if (argc != 1)
     {
-        // remainingtime = ??;
+        perror("Use: ./process <run_time>");
+        exit(EXIT_FAILURE);
     }
-    
+
+    // initialize the clk and set the previous time step
+    initClk();
+    prev_time = getClk();
+
+
+    // TODO it needs to get the remaining time from somewhere
+    remaining_time = atoi(argv[1]);
+
+    // Sleep till the scheduler wakes me up
+    raise(SIGSTOP);
+
+    while (remaining_time > 0)
+    {
+        if (getClk() == prev_time) continue;
+        remaining_time -= 1;
+        prev_time = getClk();
+    }
+
+    // Send a signal to the scheduler to inform it that this process did finish
+    kill(getppid(), SIGCHLD);
+
     destroyClk(false);
-    
+
     return 0;
 }
