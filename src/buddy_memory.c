c9404ada830f42962d2c8b05a7ea252bcd2b29a5
akramhany <akramhany65@gmail.com>
2024-05-10 14:47:10 +0300

    feat: finish implementing freeing and printing functions in the buddy system

--- src/buddy_memory.c
+++ src/buddy_memory.c
@@ -1,7 +1,5 @@
-#pragma once
-
 #include "buddy_memory.h"
-#include <math.h>
+#include "./ds/queue.h"
 
 int convert_order_to_size(uint8_t order)
 {
@@ -28,6 +26,55 @@ buddy_node_t *allocate_memory(int memory_size, buddy_tree_t *buddy_tree)
   }
 
   buddy_node_t *node = insert_buddy_node(buddy_tree->root, order);
+
+  if (!node)
+    return NULL;
+
+  node->is_free = false;
+  node->allocated_memory = malloc(pow(2, node->order));
+
+  return node;
+}
+
+void free_memory(buddy_node_t *block)
+{
+  if (!block)
+    return;
+
+  if (!block->parent) // block is the root
+  {
+    // free the allocated memory only not the block and return
+    free(block->allocated_memory);
+    return;
+  }
+
+  buddy_node_t *parent = block->parent;
+  buddy_node_t *block_to_free = NULL;
+
+  if (block == parent->left)
+    block_to_free = parent->left;
+  else
+    block_to_free = parent->right;
+
+  free(block_to_free->allocated_memory);
+  block_to_free->allocated_memory = NULL;
+  block_to_free->is_free = true;
+
+  while (parent)
+  {
+    // If both children are free, merge them.
+    if (parent->left->is_free && parent->right->is_free)
+    {
+      free(parent->left);
+      free(parent->right);
+      parent->left = NULL;
+      parent->right = NULL;
+      parent->is_free = true;
+      parent = parent->parent;
+    }
+    else
+      break;
+  }
 }
 
 buddy_tree_t *create_buddy_tree()
@@ -36,6 +83,8 @@ buddy_tree_t *create_buddy_tree()
 
   buddy_tree->max_memory_size = MAX_SIZE;
   buddy_tree->root = create_buddy_node(convert_size_to_order(MAX_SIZE));
+
+  return buddy_tree;
 }
 
 buddy_node_t *create_buddy_node(uint8_t order)
@@ -90,4 +139,16 @@ buddy_node_t *insert_buddy_node(buddy_node_t *node, uint8_t order)
 
   target_node = insert_buddy_node(node->right, order);
   return target_node;
+}
+
+void print_tree(buddy_node_t *root, int level)
+{
+  if (root == NULL)
+    return;
+  for (int i = 0; i < level; i++)
+    printf(i == level - 1 ? "|-" : "  ");
+  printf("%d", root->order);
+  printf(!root->allocated_memory ? "fr\n" : "oc\n");
+  print_tree(root->left, level + 1);
+  print_tree(root->right, level + 1);
 }
